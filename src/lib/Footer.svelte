766e614d0ccaba49bb92b1b41ffbdf562d6a7b2a
ave6990 <ave6990@ya.ru>
2022-02-08 11:21:00 +0500

    clean project

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+    <copyright>
+        Copyright © 2021 Александр Ермолаев<br>e-mail: ave6990@ya.ru
+        <br>License: MIT
+    </copyright>
+</footer>
+
+<style>
+    footer {
+        grid-column: 1;
+        grid-row: 3;
+        padding: 10px;
+        text-align: center;
+        font-size: 1em;
+        color: var(--primary-text-color);
+        background: var(--primary-color);
+    }
+</style>
